231127f666421efc5bbfa349801c1d3603e190f9
Prashant-Raghorte <raghorteprashant3@gmail.com>
2024-09-01 23:01:47 +0530

    first commit

--- log_parser.py
+++ log_parser.py
@@ -16,20 +16,16 @@ def parse_log(log_string):
     
     return parsed_data
 
+log_string = (
+    'SAC:0|Sacumen|CAAS|2021.2.0|3|MALICIOUS|High|'
+    'cat=C2 cs1Label=subcat cs1=DNS_TUNNELING cs2Label=vueUrls '
+    'cs2=https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650 '
+    'cs3Label=Tags cs3=USA,Finance cs4Label=Url '
+    'cs4=https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323 '
+    'cn1Label=severityScore cn1=900 '
+    'msg="Malicious activity was reported in CAAS= A threat intelligence rule has been automatically created in DAAS." '
+    'dhost=bad.com dst=1.1.1.1'
+)
 
-# Example usage (you can remove this if only testing):
-if __name__ == "__main__":
-    log_string = (
-        'SAC:0|Sacumen|CAAS|2021.2.0|3|MALICIOUS|High|'
-        'cat=C2 cs1Label=subcat cs1=DNS_TUNNELING cs2Label=vueUrls '
-        'cs2=https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650 '
-        'cs3Label=Tags cs3=USA,Finance cs4Label=Url '
-        'cs4=https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323 '
-        'cn1Label=severityScore cn1=900 '
-        'msg="Malicious activity was reported in CAAS= A threat intelligence rule has been automatically created in DAAS." '
-        'dhost=bad.com dst=1.1.1.1'
-    )
-
-    parsed_output = parse_log(log_string)
-    print(parsed_output)
-
+parsed_output = parse_log(log_string)
+print(parsed_output)
